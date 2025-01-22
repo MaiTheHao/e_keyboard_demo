b4be779bc84677e0b345c6df17ef2c3c5f5591bc
Hào Hay Ke <134072533+MaiTheHao@users.noreply.github.com>
2025-01-23 04:24:20 +0700

    add support for multiple font weights and hide scrollbar in ProductsTop component

--- src/app/ui/font.js
+++ src/app/ui/font.js
@@ -1,3 +1,3 @@
 import { Be_Vietnam_Pro } from 'next/font/google';
 
-export const mainFont = Be_Vietnam_Pro({subsets: ['latin'], weight: '400', style: 'normal'});
+export const mainFont = Be_Vietnam_Pro({subsets: ['latin'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'], style: 'normal'});
