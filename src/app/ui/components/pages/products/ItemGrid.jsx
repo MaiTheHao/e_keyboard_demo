bb09b4c935e300a62e4fa5f86140d78df2298553
Hào Hay Ke <134072533+MaiTheHao@users.noreply.github.com>
2025-01-22 08:26:57 +0700

    .

--- src/app/ui/components/pages/products/ItemGrid.jsx
+++ src/app/ui/components/pages/products/ItemGrid.jsx
@@ -6,6 +6,34 @@ import ItemGridSkeleton from "./ItemGridSkeleton";
 import { getProductsByQuery } from "@/lib/fetchProduct";
 import useProductsContext from "@/contexts/products/useProductsContext";
 
+const generateQuery = (filter) => {
+	const newQuery = filter
+		? Object.entries(filter).reduce((ac, [key, value]) => {
+				if (!value) return ac;
+				if (Array.isArray(value) && value.length === 0) return ac;
+				ac[key] = Array.isArray(value) ? { $in: value } : value;
+				return ac;
+		  }, {})
+		: {};
+
+	return newQuery;
+};
+
+const loadProducts = async (query, sort, maxPerPage) => {
+	let response;
+	if (sort?.customOrder) {
+		response = await getProductsByQuery(query, maxPerPage + 1, 0, {
+			customOrder: sort.customOrder,
+			field: sort.field,
+		});
+	} else {
+		const sortOption = sort?.mongoSort || { _id: -1 };
+		response = await getProductsByQuery(query, maxPerPage + 1, 0, sortOption);
+	}
+
+	return response;
+};
+
 function ItemGrid({ maxPerPage = 15 }) {
 	const [query, setQuery] = useState({});
 	const [products, setProducts] = useState([]);
@@ -16,7 +44,10 @@ function ItemGrid({ maxPerPage = 15 }) {
 	const handleLoadMore = async () => {
 		let response;
 		if (sort?.customOrder) {
-			response = await getProductsByQuery(query, maxPerPage + 1, products.length, { customOrder: sort.customOrder, field: sort.field });
+			response = await getProductsByQuery(query, maxPerPage + 1, products.length, {
+				customOrder: sort.customOrder,
+				field: sort.field,
+			});
 		} else {
 			const sortOption = sort?.mongoSort || { _id: -1 };
 			response = await getProductsByQuery(query, maxPerPage + 1, products.length, sortOption);
@@ -28,33 +59,16 @@ function ItemGrid({ maxPerPage = 15 }) {
 	};
 
 	useEffect(() => {
-		const newQuery = filter
-			? Object.entries(filter).reduce((acc, [key, value]) => {
-					if (!value) return acc;
-					if (Array.isArray(value) && value.length === 0) return acc;
-					acc[key] = Array.isArray(value) ? { $in: value } : value;
-					return acc;
-			  }, {})
-			: {};
-		setQuery(newQuery);
+		setQuery(generateQuery(filter));
 		setIsPending(true);
 	}, [filter]);
 
 	useEffect(() => {
-		async function loadProducts() {
-			let response;
-			if (sort?.customOrder) {
-				response = await getProductsByQuery(query, maxPerPage + 1, 0, { customOrder: sort.customOrder, field: sort.field });
-			} else {
-				const sortOption = sort?.mongoSort || { _id: -1 };
-				response = await getProductsByQuery(query, maxPerPage + 1, 0, sortOption);
-			}
-	
+		loadProducts(query, sort, maxPerPage).then((response) => {
 			setAbleToLoadMore(response.length > maxPerPage);
 			setProducts(response.slice(0, maxPerPage));
 			setIsPending(false);
-		}
-		loadProducts();
+		});
 	}, [query, sort]);
 
 	if (isPending) {
